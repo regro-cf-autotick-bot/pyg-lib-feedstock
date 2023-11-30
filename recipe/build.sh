94c5661266a5ec54c655b3b041bbd1eb0106e271
Hadrien Mary <hadim@users.noreply.github.com>
2023-11-29 21:17:46 -0500

    osx-arm64 attempt #2 (#23)
    * prepare for osx-arm64
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.0, and conda-forge-pinning 2023.11.27.19.54.17
    
    * start the rabbit hole
    
    * wip
    
    * macos sdk
    
    * macos sdk
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.0, and conda-forge-pinning 2023.11.27.19.54.17
    
    * wip
    
    * no torch
    
    * test again
    
    * test again
    
    * Update meta.yaml
    
    * cleaning
    
    * use prefix only
    
    * prefix only #2
    
    * Update recipe/build.sh
    
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>
    
    * Update recipe/meta.yaml
    
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>
    
    * Update recipe/build.sh
    
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>
    
    * Update recipe/build.sh
    
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>
    
    * Update recipe/meta.yaml
    
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>
    
    * address comments
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.1, and conda-forge-pinning 2023.11.29.10.04.42
    
    * remove pytorch from build section
    
    * Empty-Commit
    
    * typo
    
    * new attempt
    
    * another try
    
    * WIP
    
    * reviews
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,51 +5,52 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
   export FORCE_CUDA=1
 
   # Set the CUDA arch list from
-  # https://github.com/conda-forge/pytorch-cpu-feedstock/blob/2be0b38024b3b5601fcefce40596fc2a5fce4ab7/recipe/build_pytorch.sh#L94
+  # https://github.com/conda-forge/pytorch-cpu-feedstock/blob/main/recipe/build_pytorch.sh
 
   if [[ ${cuda_compiler_version} == 9.0* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;7.0+PTX"
-      export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;7.0+PTX"
+    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 9.2* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0+PTX"
-      export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0+PTX"
+    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 10.* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5+PTX"
-      export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5+PTX"
+    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 11.0* ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0+PTX"
-      export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0+PTX"
+    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 11.1 ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-      export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 11.2 ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-      export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 11.8 ]]; then
-      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
-      export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 12.0 ]]; then
-      export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
-      # $CUDA_HOME not set in CUDA 12.0. Using $PREFIX
-      export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
+    export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+    # $CUDA_HOME not set in CUDA 12.0. Using $PREFIX
+    export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
   else
-      echo "unsupported cuda version. edit build.sh"
-      exit 1
+    echo "unsupported cuda version. edit build.sh"
+    exit 1
   fi
 
 else
   export FORCE_CUDA=0
 fi
 
-if [[ "${target_platform}" == osx-* ]]; then
-  export CXXFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY ${CXXFLAGS}"
-fi
+# Dynamic libraries need to be lazily loaded so that torch
+# can be imported on system without a GPU
+export LDFLAGS="${LDFLAGS//-Wl,-z,now/-Wl,-z,lazy}"
 
 # export USE_MKL_BLAS=1  # only used for >0.1.0
-export Torch_DIR=$(python -c 'import torch; print(torch.utils.cmake_prefix_path)')
-
 export FORCE_NINJA=1
 export EXTERNAL_PHMAP_INCLUDE_DIR="${BUILD_PREFIX}/include/"
 export EXTERNAL_CUTLASS_INCLUDE_DIR="${BUILD_PREFIX}/include/"
 
-python -m pip install . -vvv
+# note that we patch PYG_CMAKE_ARGS into setup.py, in order to set Torch_DIR correctly also for cross-compilation
+export PYG_CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DTorch_DIR=${PREFIX}/lib/python${PY_VER}/site-packages/torch/share/cmake/Torch"
+
+${PYTHON} -m pip install . -vvv
