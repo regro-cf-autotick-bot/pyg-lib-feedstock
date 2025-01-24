4bfe15b5ff5c0a97a15b0c20382455a9554a650e
h-vetinari <h.vetinari@gmx.com>
2025-01-24 06:57:05 +0100

    add arches for CUDA 12.6, remove obsolete CUDA versions

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,30 +7,15 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
   # Set the CUDA arch list from
   # https://github.com/conda-forge/pytorch-cpu-feedstock/blob/main/recipe/build_pytorch.sh
 
-  if [[ ${cuda_compiler_version} == 9.0* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;7.0+PTX"
-    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
-  elif [[ ${cuda_compiler_version} == 9.2* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0+PTX"
-    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
-  elif [[ ${cuda_compiler_version} == 10.* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5+PTX"
-    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
-  elif [[ ${cuda_compiler_version} == 11.0* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0+PTX"
-    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
-  elif [[ ${cuda_compiler_version} == 11.1 ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
-  elif [[ ${cuda_compiler_version} == 11.2 ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
-  elif [[ ${cuda_compiler_version} == 11.8 ]]; then
+  if [[ ${cuda_compiler_version} == 11.8 ]]; then
     export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
     export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
   elif [[ ${cuda_compiler_version} == 12.0 ]]; then
     export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
     # $CUDA_HOME not set in CUDA 12.0. Using $PREFIX
+    export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
+  elif [[ ${cuda_compiler_version} == 12.6 ]]; then
+    export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
     export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
   else
     echo "unsupported cuda version. edit build.sh"
